89aab806a43580e046ec122b281031b89ac90881
DrKrakus <jk.noox@gmail.com>
2018-05-18 13:24:19 +0200

    Initial Commit

---
+++ SwiftGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftGame
+//
+//  Created by Jerome Krakus on 18/05/2018.
+//  Copyright © 2018 Jerome Krakus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
